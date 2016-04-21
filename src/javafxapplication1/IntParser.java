2dc556e0ed7a63624450fcae4a6eb2f0e8646fdc
jaceksen <jaceksen@gmail.com>
2016-04-21 10:15:13 +0200

    pierwszy commit work

---
+++ src/javafxapplication1/IntParser.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication1;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jsen
+ */
+public interface IntParser {
+
+    void start(Stage primaryStage);
+    
+}
