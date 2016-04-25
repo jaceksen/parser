b6b070a9e8d1448a517b4ea4eb9211cca45b1ea1
jaceksen <jaceksen@gmail.com>
2016-04-25 10:18:12 +0200

    commit changes parser

--- src/javafxapplication1/IntParser.java
+++ src/javafxapplication1/IntParser.java
@@ -3,10 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<<<<<<< HEAD
-=======
 
->>>>>>> 7f812e51e0b3076a8a2fcf4691ddd31747ac13c0
 package javafxapplication1;
 
 import javafx.stage.Stage;
